a78b0ee6028e6a75bc4cb25a35c077aa17ab1a9c
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-09-26 12:33:29 -0700

    feat: Add routes '/', '/interviewer'

--- src/app/interviewer/page.tsx
+++ src/app/interviewer/page.tsx
@@ -1,9 +1,8 @@
-export default function Home() {
+export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h1 className="text-3xl font-bold py-4 underline text-blue-400">Eval AI</h1>
-      <p className="text-base font-semibold">Under Construction! Coming Soon. (Interviewee Page)</p>
+      <p className="text-base font-semibold">Under Construction! Coming Soon. (Interviewer Page)</p>
     </div>
   );
 }
-
